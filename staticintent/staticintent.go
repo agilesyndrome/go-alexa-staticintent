b4298d7ecc42da03a0b2b6947acb88f896884736
Drew Easley <drew.easley@logikcull.com>
2018-11-03 19:01:11 -0400

    Update to fallback to title

--- staticintent/staticintent.go
+++ staticintent/staticintent.go
@@ -3,6 +3,7 @@ package staticintent
 import (
   "github.com/arienmalec/alexa-go"
   "github.com/agilesyndrome/go-alexa-i18n/alexai18n"
+  "strings"
 )
 
 func Simple(request alexa.Request, intent_name string) alexa.Response {
@@ -14,9 +15,18 @@ func Handler(request alexa.Request) (alexa.Response) {
 
         intent_name := request.Body.Intent.Name
 	if ( intent_name == "" ) {
-	  intent_name = "not-specified"
+	  intent_name = "skill-name"
 	}
+
+
 	title := alexai18n.WorldString(request, intent_name + ".title")
         text := alexai18n.WorldString(request, intent_name + ".text")
+
+        //Should the title be missing, and we end up with an id instead of actual text
+	//then fallback to the skill-name as the title
+        if ( strings.HasSuffix(title, ".title") ) {
+	  title = alexai18n.WorldString(request, "skill-name")
+	}
+
         return alexa.NewSimpleResponse(title, text)
 }
